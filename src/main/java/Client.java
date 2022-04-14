a49d46e9e2b6fe14e3890a31804fdcf4aba3ad81
DA_Laer <laerdanil@mail.ru>
2022-04-14 15:29:10 +0300

    task2

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,16 +5,26 @@ public class Client {
     public static void main(String[] args) throws IOException {
 
         String host = "localhost";
-        int port = 8080;
+        int port = 8081;
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-            out.printf("GET / HTTP/1.1\nHost: localhost\nport: %d ",clientSocket.getPort());
+            String name = in.readLine();
+            System.out.println(name);
+
+            out.println("Vac");
+
+            String question1 = in.readLine();
+            System.out.println(question1);
+
+            out.println("yes");
 
             String resp = in.readLine();
-
             System.out.println(resp);
+
+
+
         }
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,20 +4,30 @@ import java.net.*;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        int port = 8080;
+        int port = 8081;
         ServerSocket serverSocket = new ServerSocket(port);
-        Socket clientSocket = serverSocket.accept(); // ждем подключения
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        System.out.println("New connection accepted");
-        String name = in.readLine();
-        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-        String msg = String.format("Hir name: %s\nHir port is %d", name, clientSocket.getPort());
 
-        System.out.println(msg);
-        serverSocket.close();
+            Socket clientSocket = serverSocket.accept(); // ждем подключения
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            System.out.println("New connection accepted");
+            out.println("Write your name");
+            String name = in.readLine();
+            out.println("Are you child? (yes/no)");
+
+            String answer1 = in.readLine();
+            System.out.println(answer1);
+            if (answer1.equals("yes")) {
+                out.println("Welcome to the kids area, " + name + "! Let's play!");
+            } else if (answer1.equals("no")) {
+                out.println("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!");
+            } else {
+                out.println("Error");
+                serverSocket.close();
+            }
+            serverSocket.close();
+
     }
-
-
 }
